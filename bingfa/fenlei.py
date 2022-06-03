60966bf28cd304d56d6880458bfc7544d71b22a5
lcc-666 <2502247952@qq.com>
2022-06-03 19:20:08 +0800

    将图片按照横竖屏来分类

--- bingfa/fenlei.py
+++ bingfa/fenlei.py
@@ -1,35 +1,40 @@
-#将图片横竖屏分类
-
 import os
 from PIL import Image
 import shutil
-from concurrent.futures import ThreadPoolExecutor,as_completed
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import time
 
+"""
+将图片按照横竖屏来分类
+"""
+
+
 def getsize(i):
-    img=Image.open(i)
-    w=img.width
-    h=img.height
-    if w>h:
-        shutil.move(i,'./width')
+    img = Image.open(i)
+    w = img.width
+    h = img.height
+    if w > h:
+        shutil.move(i, './width')
     else:
-        shutil.move(i,'./heigth')
+        shutil.move(i, './heigth')
+
 
 def main():
-    start=time.time()
-    file='./bt/'
-    ls=os.listdir(file)
+    start = time.time()
+    file = './bt/'
+    ls = os.listdir(file)
     os.mkdir('./width')
     os.mkdir('./heigth')
     # for i in ls:
     #     getsize(file+i)
-    with ThreadPoolExecutor(max_workers=128)as executor:
+    with ThreadPoolExecutor(max_workers=128) as executor:
         future_list = []
         for i in ls:
-            future = executor.submit(getsize, file+i)
+            future = executor.submit(getsize, file + i)
             future_list.append(future)
         as_completed(future_list)
     end = time.time()
     print(end - start)
+
 
 main()
