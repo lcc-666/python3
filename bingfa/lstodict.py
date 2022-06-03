166db3710502836ecebb02898b1d6055f72a1418
lcc-666 <2502247952@qq.com>
2022-06-03 19:36:30 +0800

    词频统计

---
+++ bingfa/lstodict.py
@@ -0,0 +1,21 @@
+"""
+列表转字典并统计个数
+可传入已经统计过的字典
+"""
+
+
+def todict(ls, dict=None):
+    if dict is None:
+        dict = {}
+    for i in ls:
+        if i in dict:
+            dict[i] += 1
+        else:
+            dict[i] = 1
+    return dict
+
+
+if __name__ == '__main__':
+    ls = [5, 5, 6, 6, 7, 8, 8, 9]
+    dt = todict(ls)
+    print(dt)
